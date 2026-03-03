25e09477bd2f4f1cdc9e70ca99ae7a101d8d5ee5
Vixhesh <shuklavishesh470@gmail.com>
2026-03-03 11:37:23 +0530

    Node Js Updated for deployment

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,17 +1,22 @@
-const express=require("express");
-const jwt=require("jsonwebtoken");
-const app= express();
-const secret="supersecret";
-app.use(express.static("public"));
-app.get('/' ,(req,res)=>{
-    const token=req.query.token;
-    try{
-        jwt.verify(token.secret)
-        res.sendFile(__dirname+"/public/index.html");
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
-    }catch{
+const app = express();
+const secret = process.env.SECRET_KEY; 
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    const token = req.query.token;
+
+    try {
+        jwt.verify(token, secret);
+        res.sendFile(__dirname + "/public/index.html");
+    } catch {
         res.status(401).send("Unauthorized");
     }
-
 });
-app.listen(3000, () => console.log("Game server on 3000"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Game server running on ${PORT}`));
